10b1ec5465ebaa5251fa89b98446eb2b0fa4f337
Eric Charnesky <eric.charnesky@careevolution.com>
2021-03-10 23:11:28 -0500

    remote comments

--- MidtermReview/src/midtermreview/StuffedAnimal.java
+++ MidtermReview/src/midtermreview/StuffedAnimal.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package midtermreview;
 
-/**
- *
- * @author EricC
- */
 public abstract class StuffedAnimal {
     
     private boolean batteryHasPower;
