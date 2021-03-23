2d406f89f6ef4f8af6d70955997d8e169bcc47be
Eric Charnesky <eric.charnesky@careevolution.com>
2021-03-23 10:16:30 -0400

    project 3 morning

---
+++ Project3Morning/src/project3/Bend.java
@@ -0,0 +1,6 @@
+package project3;
+
+public class Bend {
+    private int maxSpeed;
+    private int positionOnTrack;
+}
---
+++ Project3Morning/src/project3/Cart.java
@@ -0,0 +1,14 @@
+
+package project3;
+
+public abstract class Cart {
+    
+    public Cart(int maxSpeed, int acceleration, int breakSpeed)
+    {
+        
+    }
+    
+    public void enterBend(Bend bend){
+        
+    }
+}
